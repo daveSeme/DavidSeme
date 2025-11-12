c70d608a6c9ad97cc687c94819cdb3f317a3fd10
David Seme <davidluvai56@gmail.com>
2025-11-12 22:44:00 +0300

    update for teh latest

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// Terminal Typing Effect
-const phrases = ["Software Engineer", "Data Analyst", "STEM Educator"];
-let currentPhrase = 0;
-let currentLetter = 0;
-let isDeleting = false;
-const typingText = document.getElementById('typing-text');
+// // Terminal Typing Effect
+// const phrases = ["Software Engineer", "Data Analyst", "STEM Educator"];
+// let currentPhrase = 0;
+// let currentLetter = 0;
+// let isDeleting = false;
+// const typingText = document.getElementById('typing-text');
 
-function type() {
-  const currentText = phrases[currentPhrase];
+// function type() {
+//   const currentText = phrases[currentPhrase];
   
-  if (!isDeleting) {
-    typingText.textContent = currentText.substring(0, currentLetter + 1);
-    currentLetter++;
+//   if (!isDeleting) {
+//     typingText.textContent = currentText.substring(0, currentLetter + 1);
+//     currentLetter++;
     
-    if (currentLetter === currentText.length) {
-      isDeleting = true;
-      setTimeout(type, 1000);
-      return;
-    }
-  } else {
-    typingText.textContent = currentText.substring(0, currentLetter - 1);
-    currentLetter--;
+//     if (currentLetter === currentText.length) {
+//       isDeleting = true;
+//       setTimeout(type, 1000);
+//       return;
+//     }
+//   } else {
+//     typingText.textContent = currentText.substring(0, currentLetter - 1);
+//     currentLetter--;
     
-    if (currentLetter === 0) {
-      isDeleting = false;
-      currentPhrase = (currentPhrase + 1) % phrases.length;
-    }
-  }
+//     if (currentLetter === 0) {
+//       isDeleting = false;
+//       currentPhrase = (currentPhrase + 1) % phrases.length;
+//     }
+//   }
   
-  setTimeout(type, isDeleting ? 50 : 100);
-}
+//   setTimeout(type, isDeleting ? 50 : 100);
+// }
 
-// Start animation
-type();
+// // Start animation
+// type();
